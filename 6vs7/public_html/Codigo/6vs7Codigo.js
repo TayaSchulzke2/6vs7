6391bc7591541d3de4fa574f53d6ad83e5a1e13b
PID <PID@DESKTOP-15JEJLJ>
2026-05-11 13:04:23 +0200

    avanzar la parte del 6

--- 6vs7/public_html/Codigo/6vs7Codigo.js
+++ 6vs7/public_html/Codigo/6vs7Codigo.js
@@ -1,15 +1,32 @@
 "use-strict";
 
 let personatge; 
+let pregunta;
 let robot;
 let missatge; 
+let vida6=67;
+let vida7=67;
+let atacar;
+let curar;
 
 
 personatge= prompt("Qui vols ser, el 6 o el 7?"); 
 if(personatge == "6"){
     robot="7";
-    alert("vida inicial:\n 6 : 67\n 7:67 ");
+    alert("vida:\n 6:"+vida6+"\n 7:"+vida7);
+    pregunta=prompt("Vols atacar o curar-te? (tot en minúscula)");
+    pregunta.toLowerCase();
+    pregunta.trim();
+    if (pregunta=="atacar"){
+        atacar=
+        vida7=vida7-atacar;
+    }else if(pregunta=="curar"){
+        
+    }else{
+        
+    }
+    
 }else{
     robot= "6"; 
-     alert("vida inicial:\n 6 : 67\n 7:67 ");
+     alert("vida:\n 6:"+vida6+"\n 7:"+vida7);
 }
