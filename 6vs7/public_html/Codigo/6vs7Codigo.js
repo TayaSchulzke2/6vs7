2a70fbeeb3dbf9df3a413900ccbe29377346e22d
PID <PID@DESKTOP-C81H4QS>
2026-05-11 12:43:26 +0200

    començar el codi

--- 6vs7/public_html/Codigo/6vs7Codigo.js
+++ 6vs7/public_html/Codigo/6vs7Codigo.js
@@ -1,6 +1,15 @@
 "use-strict";
 
-numero=parseInt(Math.random() * 100 + 1);
-alert(numero);
+let personatge; 
+let robot;
+let missatge; 
 
 
+personatge= prompt("Qui vols ser, el 6 o el 7?"); 
+if(personatge == "6"){
+    robot="7";
+    alert("vida inicial:\n 6 : 67\n 7:67 ");
+}else{
+    robot= "6"; 
+     alert("vida inicial:\n 6 : 67\n 7:67 ");
+}
